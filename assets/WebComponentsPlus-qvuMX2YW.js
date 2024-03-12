d60f28202e9d0cc5ef90954051cb63e319b6a79b
gaojie <471089443@qq.com>
2024-03-12 19:35:40 +0800

    update

---
+++ assets/WebComponentsPlus-qvuMX2YW.js
@@ -0,0 +1,141 @@
+import{r as d,e as z,f as ne,o as t,c as o,d as e,t as h,F as v,g as $,h as u,i as se,j as ae,p as B,b as D,k as ie,l as de,w as b,m as y,u as ue,a as ce,n as V}from"./index-Eb1R7_MP.js";import{_ as T,H as re}from"./Header-MuDYq3jn.js";/* empty css                                                                              */const I="/default.png",s=p=>(B("data-v-f9e7642a"),p=p(),D(),p),pe={class:"wcp-demo-container"},he={class:"demo-row"},ve=["disabled"],me=["disabled"],_e={class:"demo-row"},ge=s(()=>e("w-input",{placeholder:"username",style:{"margin-right":"10px","margin-bottom":"10px"}},null,-1)),we=s(()=>e("w-input",{type:"password",style:{"margin-right":"10px","margin-bottom":"10px"},placeholder:"password"},null,-1)),be=s(()=>e("w-input",{type:"date",value:"2024-01-01",style:{"margin-right":"10px","margin-bottom":"10px"}},null,-1)),ye=s(()=>e("w-input",{type:"time",value:"00:00",style:{"margin-right":"10px","margin-bottom":"10px"}},null,-1)),fe=["value"],xe=s(()=>e("w-input",{type:"number",style:{width:"80px","margin-right":"10px","margin-bottom":"10px"},min:"0",placeholder:"age"},null,-1)),ke=["value"],Ce=s(()=>e("optgroup",{label:"Gender"},[e("option",{value:"1"},"male"),e("option",{value:"2"},"female"),e("option",{value:"3",disabled:""},"other")],-1)),$e=[Ce],je=["value"],We={class:"demo-row"},Se=["value"],Ae=s(()=>e("w-radio",{value:"1"},"male",-1)),Be=s(()=>e("w-radio",{value:"2"},"female",-1)),De=s(()=>e("w-radio",{disabled:"",value:"3"},"other",-1)),Te=[Ae,Be,De],Ie={class:"demo-row"},Pe=["value"],Le={class:"demo-row"},Re={class:"demo-row"},Ee=["value"],He=["value"],Me={class:"demo-row"},Ne=["value"],Ve=s(()=>e("w-tab",{value:"tab1"},"tab1",-1)),ze=s(()=>e("w-tab",{value:"tab2"},"tab2",-1)),qe=s(()=>e("w-tab",{value:"tab3",disabled:""},"tab3",-1)),Fe=[Ve,ze,qe],Ge={class:"demo-row"},Ue=ie('<div slot="header" data-v-f9e7642a>header</div><div slot="body" style="display:flex;flex-direction:column;overflow:hidden;" data-v-f9e7642a><div data-v-f9e7642a>body</div><div style="overflow-y:auto;" data-v-f9e7642a><div style="height:400px;" data-v-f9e7642a>scroll</div></div></div><div slot="footer" data-v-f9e7642a><div style="display:flex;width:100%;justify-content:center;" data-v-f9e7642a> footer </div></div>',3),Je=[Ue],Ke={class:"demo-row"},Oe={style:{position:"relative"}},Qe=["show"],Xe=s(()=>e("w-menu-item",{style:{"border-top":"1px solid #ddd"},disabled:""},"menu c",-1)),Ye={class:"demo-row"},Ze=s(()=>e("w-btn",null,"upload",-1)),et=[Ze],tt=s(()=>e("w-btn",null,"accept multiple",-1)),ot=[tt],lt=s(()=>e("w-upload",{url:"/api/upload",disabled:""},[e("w-btn",{disabled:""},"disabled")],-1)),nt={class:"demo-row"},st={style:{"background-color":"#fff","margin-top":"10px"}},at=["columns"],it=s(()=>e("w-table-item",{style:{"background-color":"black",color:"#fff"}},"name",-1)),dt=s(()=>e("w-table-item",{style:{"background-color":"rgb(190, 15, 15)",color:"#fff"}},"age",-1)),ut=s(()=>e("w-table-item",{style:{"background-color":"rgb(59, 60, 153)",color:"#fff"}},"interest",-1)),ct=[it,dt,ut],rt={style:{height:"100px","overflow-y":"auto"}},pt=["columns"],ht=s(()=>e("div",{class:"demo-row"},[e("w-player",{style:{width:"400px",height:"260px","max-width":"100%"},src:"https://cdn.openai.com/tmp/s/interp/e1.mp4"})],-1)),vt=s(()=>e("div",{class:"demo-row"},[e("w-slide",{style:{width:"400px",height:"300px","max-width":"100%"},auto:""},[e("w-slide-item",{style:{"background-color":"#000",color:"#fff"}},[e("div",{item:"1",style:{display:"flex","justify-content":"center","align-items":"center",width:"100%",height:"100%"}}," 1 ")]),e("w-slide-item",{style:{"background-color":"#333",color:"#fff"}},[e("div",{item:"2",style:{display:"flex","justify-content":"center","align-items":"center",width:"100%",height:"100%"}}," 2 ")]),e("w-slide-item",{style:{"background-color":"#666",color:"#fff"}},[e("div",{item:"3",style:{display:"flex","justify-content":"center","align-items":"center",width:"100%",height:"100%"}}," 3 ")])])],-1)),mt={class:"demo-row"},_t={style:{width:"400px",height:"300px","max-width":"100%"}},gt={style:{display:"flex",height:"100%"}},wt={style:{height:"100%"},src:I,"lazy-src":"/bg.png"},bt={class:"demo-row"},yt={style:{display:"flex"}},ft={style:{display:"inline-flex",height:"100%",width:"100%"}},xt=s(()=>e("img",{style:{height:"100%"},src:I},null,-1)),kt={class:"demo-row"},Ct={style:{display:"flex","flex-direction":"column"}},$t={style:{display:"inline-flex"}},jt=s(()=>e("img",{style:{width:"100%"},src:I},null,-1)),Wt=[jt],St=s(()=>e("div",{slot:"loading",style:{"text-align":"center"}},"loading",-1)),At={__name:"WebComponentsPlusDemo",setup(p){const a=d(""),_=d("#1abc8c"),g=d("1"),c=d("like football and cooking"),m=d("1"),f=d("on"),j=d(["apple","banana","orange"]),w=d([]),S=d([]),x=d("tab1"),P=d(null),A=d(null),L=d(null),k=d(null),W=d(.2),R=d([{name:"pony",age:18,interest:"football"}]);for(let l=0;l<100;l++)R.value.push({name:"pony",age:18,interest:"football"});const E=d("100px 100px 1fr"),H=d([{}]),M=d([{}]);function F(l){_.value=l.detail.value,se.initColor(_.value)}function G(l){g.value=l.detail.value}function U(l){c.value=l.detail.value}function J(l){m.value=l.detail.value}function K(l){let n=l.target.getAttribute("value");if(l.target.hasAttribute("checked"))w.value.push(n);else{let i=w.value.filter((C,mo)=>C!=n);w.value.length=0,w.value.push(...i)}}function O(l){l.target.hasAttribute("checked")?(w.value.length=0,S.value.forEach(n=>{n.setAttribute("checked",""),w.value.push(n.getAttribute("value"))})):(w.value.length=0,S.value.forEach(n=>{n.removeAttribute("checked")}))}function Q(l){f.value=l.detail.value}function X(l){console.log("tabs change",l.detail.value),x.value=l.detail.value}function Y(l){A.value.setAttribute("show","")}function Z(l){L.value.setAttribute("show","")}function ee(l){k.value=k.value==""?null:""}function te(){ae("delete?",l=>{P.value.show("success")})}function oe(){H.value.push({})}function le(){M.value.push({})}async function N(l){console.log(l.detail)}return z(()=>{}),ne(()=>{}),(l,n)=>(t(),o("div",pe,[e("div",he,[e("w-btn",{style:{"margin-right":"10px"},onClick:n[0]||(n[0]=i=>a.value=a.value==""?null:"")},"default"),e("w-btn",{style:{"margin-right":"10px"},type:"outline",disabled:a.value},"outline",8,ve),e("w-btn",{type:"text",disabled:a.value},"text",8,me)]),e("div",_e,[ge,we,be,ye,e("w-input",{type:"color",value:_.value,onChange:F,style:{width:"40px",height:"35px","margin-right":"10px","margin-bottom":"10px"}},null,40,fe),xe,e("w-select",{value:g.value,onChange:G,style:{"margin-right":"10px","margin-bottom":"10px"}},$e,40,ke),e("w-textarea",{value:c.value,onChange:U},null,40,je),e("div",null,h(c.value),1)]),e("div",We,[e("w-radios",{value:m.value,onChange:J},Te,40,Se),e("div",null,h(m.value),1)]),e("div",Ie,[(t(!0),o(v,null,$(j.value,i=>(t(),o("w-checkbox",{ref_for:!0,ref_key:"checkboxRef",ref:S,value:i,onChange:n[1]||(n[1]=C=>K(C))},h(i),41,Pe))),256)),e("w-checkbox",{onChange:O},"all",32),e("div",null,h(w.value),1)]),e("div",Le,[e("w-toggle",{on:"",onChange:Q},null,32),e("div",null,h(f.value),1)]),e("div",Re,[e("w-heart",{value:W.value,onChange:n[2]||(n[2]=i=>{W.value=i.detail.value,console.log(i.detail.value)})},null,40,Ee),e("w-heart",{disabled:"",value:W.value},null,8,He),e("div",null,h(W.value),1)]),e("div",Me,[e("w-tabs",{value:x.value,onChange:X},Fe,40,Ne),e("div",null,[x.value=="tab1"?(t(),o(v,{key:0},[u(" tab1 ")],64)):x.value=="tab2"?(t(),o(v,{key:1},[u(" tab2 ")],64)):x.value=="tab3"?(t(),o(v,{key:2},[u(" tab3 ")],64)):(t(),o(v,{key:3},[],64))])]),e("div",Ge,[e("w-btn",{onClick:Y},"modal"),e("w-btn",{onClick:Z,style:{margin:"0 10px"}},"dialog"),e("w-btn",{onClick:te},"confirm"),e("w-msg",{ref_key:"msg",ref:P},null,512),e("w-modal",{ref_key:"modal",ref:A},[e("div",null,[e("w-btn",{onClick:n[3]||(n[3]=i=>A.value.removeAttribute("show"))},"hide")])],512),e("w-dialog",{ref_key:"dialog",ref:L},Je,512)]),e("div",Ke,[e("div",Oe,[e("w-btn",{onClick:ee},"menu"),e("w-menu",{show:k.value,style:{top:"40px",left:"0"}},[e("w-menu-item",{onClick:n[4]||(n[4]=i=>k.value=null)},"item a"),e("w-menu-item",{onClick:n[5]||(n[5]=i=>k.value=null)},"item b"),Xe],8,Qe)])]),e("div",Ye,[e("w-upload",{onChange:n[6]||(n[6]=i=>N(i)),url:"/api/upload"},et,32),e("w-upload",{style:{margin:"0 10px"},onChange:n[7]||(n[7]=i=>N(i)),url:"/api/upload",accept:".png,.jpg",multiple:"",name:"img"},ot,32),lt]),e("div",nt,[e("div",st,[e("div",null,[e("w-table-row",{columns:E.value},ct,8,at)]),e("div",rt,[(t(!0),o(v,null,$(R.value,i=>(t(),o("w-table-row",{columns:E.value,hover:""},[e("w-table-item",null,h(i.name),1),e("w-table-item",null,h(i.age),1),e("w-table-item",null,h(i.interest),1)],8,pt))),256))])])]),ht,vt,e("div",mt,[e("w-lazy-loading",_t,[e("div",gt,[(t(),o(v,null,$([1,2,3],i=>e("img",wt)),64))])])]),e("div",bt,[e("w-scroll-loading",{style:{width:"400px",height:"300px","max-width":"100%"},onChange:oe},[e("div",yt,[(t(!0),o(v,null,$(H.value,(i,C)=>(t(),o("div",ft,[xt,u(" "+h(C),1)]))),256))])],32)]),e("div",kt,[e("w-scroll-loading",{style:{width:"400px",height:"300px","max-width":"100%","flex-direction":"column"},onChange:le},[e("div",Ct,[(t(!0),o(v,null,$(M.value,i=>(t(),o("div",$t,Wt))),256))]),St],32)])]))}},Bt=T(At,[["__scopeId","data-v-f9e7642a"]]),r=p=>(B("data-v-0805e053"),p=p(),D(),p),Dt={class:"wcp-doc-container"},Tt=["value"],It=r(()=>e("w-tab",{value:"install"},"install",-1)),Pt=r(()=>e("w-tab",{value:"w-btn"},"w-btn",-1)),Lt=r(()=>e("w-tab",{value:"w-input"},"w-input",-1)),Rt=[It,Pt,Lt],Et=r(()=>e("h3",null,"npm",-1)),Ht=r(()=>e("code",null,"npm install web-components-plus",-1)),Mt=r(()=>e("code",null,`import { Core, WBtn } from 'web-components-plus'
+
+Core.initColor()
+`,-1)),Nt=r(()=>e("h3",null,"browser",-1)),Vt=r(()=>e("code",null,`import { Core, WBtn } from 'https://unpkg.com/web-components-plus@1/main.js'
+
+Core.initColor()
+`,-1)),zt=r(()=>e("h3",null,"w-btn",-1)),qt=["value"],Ft={key:0},Gt=["value"],Ut=r(()=>e("w-tab",{value:"w-player"},"w-player",-1)),Jt=r(()=>e("w-tab",{value:"w-scroll-loading"},"w-scroll-loading",-1)),Kt=r(()=>e("w-tab",{value:"w-heart"},"w-heart",-1)),Ot=[Ut,Jt,Kt],Qt=r(()=>e("h3",null,"w-player",-1)),Xt=r(()=>e("h3",null,"w-scroll-loading",-1)),Yt=r(()=>e("h3",null,"w-heart",-1)),Zt={key:3},eo=`// js
+<html>
+    <body>
+        <w-btn>default</w-btn>
+        <w-btn type="outline">outline</w-btn>
+        <w-btn type="text">outline</w-btn>
+        <script type="module">
+            import { WBtn } from 'https://unpkg.com/web-components-plus@1/main.js'
+
+        <\/script>
+    </body>
+</html>
+// vue
+<script setup>
+    import { WBtn } from 'web-components-plus'
+
+    const btnType = ref('outline')
+    function changeBtn(e) {
+        btnType.value = btnType.value == 'outline' ? 'text' : 'outline'
+    }
+<\/script>
+<template>
+    <w-btn @click="changeBtn" :type="btnType">default</w-btn>
+</template>`,to=`// js
+<html>
+<body>
+    <w-player style="width: 400px; height: 300px;" src="player.mp4" poster="player.jpg"></w-player>
+    <script  type="module">
+        import { WPlayer } from 'https://unpkg.com/web-components-plus@1/main.js'
+
+    <\/script>
+</body>
+</html>
+// vue
+<script setup>
+    import { WPlayer } from 'web-components-plus'
+
+<\/script>
+<template>
+    <w-player style="width: 400px; height: 300px;" src="player.mp4" poster="player.jpg"></w-player>
+</template>`,oo=`// js
+<html>
+<body>
+    <w-scroll-loading style="width: 300px; height: 300px; flex-direction: column;" id="scroll2">
+        <div id="list2" style="display: flex; flex-direction: column;">
+            <div class="item">
+                <img style="width: 100%;" src="default.png" />
+            </div>
+        </div>
+        <div slot="loading" style="text-align: center;">loading</div>
+    </w-scroll-loading>
+    <script  type="module">
+        import { WScrollLoading } from 'https://unpkg.com/web-components-plus@1/main.js'
+
+        document.querySelector('#scroll').addEventListener('change', (e) => {
+            // loadDate
+            document.querySelector('#list').innerHTML += \`<div class="item"><img style="height:100%;" src="default.png" /></div>\`
+        })
+    <\/script>
+</body>
+</html>
+// vue
+<script setup>
+    import { WScrollLoading } from 'web-components-plus'
+
+    const list = ref([{}])
+    function loading() {
+        list.value.push({})
+    }
+<\/script>
+<template>
+    <w-scroll-loading style="width: 300px; height: 300px; flex-direction: column;" @change="loading">
+        <div style="display: flex; flex-direction: column;">
+            <div style="display: inline-flex;" v-for="i in list">
+                <img style="width: 100%;" src="/default.png" />
+            </div>
+        </div>
+        <div slot="loading" style="text-align: center;">loading</div>
+    </w-scroll-loading>
+</template>`,lo=`// js
+<html>
+    <body>
+        <w-heart id="heart" value="0"></w-heart>
+        <script type="module">
+            import { WHeart } from 'https://unpkg.com/web-components-plus@1/main.js'
+
+            document.querySelector('#heart').addEventListener('change',(e)=>{
+                console.log(e.detail.value)
+            })
+        <\/script>
+    </body>
+</html>
+// vue
+<script setup>
+    import { WHeart } from 'web-components-plus'
+
+    const value = ref(0)
+    function change(e) {
+        value.value = e.detail.value
+    }
+<\/script>
+<template>
+    <w-heart :value="value" @change="change"></w-heart>
+</template>`,no={__name:"WebComponentsPlusDoc",setup(p){const a=d({code:"",base:"install",moneyComponent:"w-player"});return z(_=>{}),(_,g)=>{const c=de("highlight");return t(),o("div",Dt,[e("w-tabs",{value:a.value.base,onChange:g[0]||(g[0]=m=>{a.value.base=m.detail.value})},Rt,40,Tt),e("div",null,[a.value.base=="install"?(t(),o(v,{key:0},[Et,b((t(),o("pre",null,[u("                        "),Ht,u(`
+                        `),Mt,u(`
+                `)])),[[c]]),Nt,b((t(),o("pre",null,[u("                        "),Vt,u(`
+                `)])),[[c]])],64)):y("",!0),a.value.base=="w-btn"?(t(),o(v,{key:1},[zt,b((t(),o("pre",null,[u("                        "),e("code",null,h(eo)),u(`
+| attr | value |
+| --- | --- |
+| type | default/outline/text |
+| color | default/success/error/warning |
+| size | default/sm/lg |
+| disabled | boolean |
+                    `)])),[[c]])],64)):y("",!0)]),e("w-input",{style:{width:"300px"},placeholder:"input code show more",value:a.value.code,onChange:g[1]||(g[1]=m=>{a.value.code=m.detail.value})},null,40,qt),a.value.code=="js-sh"?(t(),o("div",Ft,[e("w-tabs",{value:a.value.moneyComponent,onChange:g[2]||(g[2]=m=>{a.value.moneyComponent=m.detail.value})},Ot,40,Gt),e("div",null,[a.value.moneyComponent=="w-player"?(t(),o(v,{key:0},[Qt,b((t(),o("pre",null,[u("                        "),e("code",null,h(to)),u(`
+| attr | value |
+| --- | --- |
+| src | string |
+| poster | string |
+
+| method | value |
+| --- | --- |
+| play |  |
+| pause |  |
+                    `)])),[[c]])],64)):a.value.moneyComponent=="w-scroll-loading"?(t(),o(v,{key:1},[Xt,b((t(),o("pre",null,[u("                        "),e("code",null,h(oo)),u(`
+| attr | value |
+| --- | --- |
+| change | event |
+                    `)])),[[c]])],64)):a.value.moneyComponent=="w-heart"?(t(),o(v,{key:2},[Yt,b((t(),o("pre",null,[u("                        "),e("code",null,h(lo)),u(`
+| attr | value |
+| --- | --- |
+| value | number |
+| disabled | boolean |
+| size | sm (default) lg |
+| change | event |
+                    `)])),[[c]])],64)):a.value.moneyComponent=="w-slide"?(t(),o("h3",Zt,"w-slide")):y("",!0)])])):y("",!0)])}}},so=T(no,[["__scopeId","data-v-0805e053"]]),q=p=>(B("data-v-8c59b100"),p=p(),D(),p),ao={class:"wrap"},io={class:"content"},uo=["value"],co=q(()=>e("w-tab",{value:"demo"},"demo",-1)),ro=q(()=>e("w-tab",{value:"doc"},"doc",-1)),po=[co,ro],ho={class:"tab-content"},vo={__name:"WebComponentsPlus",setup(p){ue();const a=d(null),_=d({tab:"demo"});return(g,c)=>(t(),o("div",{class:"wcp-container",onClick:c[1]||(c[1]=(...m)=>{var f,j;return((f=a.value)==null?void 0:f.hideMenu)&&((j=a.value)==null?void 0:j.hideMenu(...m))})},[ce(re,{ref_key:"headerRef",ref:a},null,512),e("div",ao,[e("div",io,[e("w-tabs",{value:_.value.tab,onChange:c[0]||(c[0]=m=>{_.value.tab=m.detail.value})},po,40,uo),e("div",ho,[_.value.tab=="demo"?(t(),V(Bt,{key:0})):y("",!0),_.value.tab=="doc"?(t(),V(so,{key:1})):y("",!0)])])])]))}},bo=T(vo,[["__scopeId","data-v-8c59b100"]]);export{bo as default};
