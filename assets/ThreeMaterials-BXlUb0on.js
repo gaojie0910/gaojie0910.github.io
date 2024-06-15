bfa0d46b5ad4bfc3f878abc49f84fa626228ba70
gaojie <471089443@qq.com>
2024-06-15 18:20:01 +0800

    update

--- assets/ThreeMaterials-BXlUb0on.js
+++ assets/ThreeMaterials-BXlUb0on.js
@@ -1,4 +1,4 @@
-import{_ as bf}from"./Header-wOt6XhLZ.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-tjoG9nha.js";/**
+import{_ as bf}from"./Header-BQ3P4GvG.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-DR4dYa5m.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
