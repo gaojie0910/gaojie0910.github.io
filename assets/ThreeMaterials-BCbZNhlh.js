c7652a10ece07c9cd945557dc07613448139c2c5
gaojie <471089443@qq.com>
2024-07-11 17:37:40 +0800

    update

--- assets/ThreeMaterials-BCbZNhlh.js
+++ assets/ThreeMaterials-BCbZNhlh.js
@@ -1,4 +1,4 @@
-import{_ as bf}from"./Header-Ds9a2Jff.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-Ff97l25p.js";/**
+import{_ as bf}from"./Header-496qnFgZ.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-CRJHnJ05.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
