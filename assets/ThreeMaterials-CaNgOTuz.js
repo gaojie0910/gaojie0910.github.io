bf9165c2e541f59e84573312266e98e2a19dc242
gaojie <471089443@qq.com>
2024-07-29 18:59:31 +0800

    update

--- assets/ThreeMaterials-CaNgOTuz.js
+++ assets/ThreeMaterials-CaNgOTuz.js
@@ -1,4 +1,4 @@
-import{_ as bf}from"./Header-BHs1vE3g.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-DazVQd6w.js";/**
+import{_ as bf}from"./Header-PV0toX5M.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-D9lw3Zhd.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
