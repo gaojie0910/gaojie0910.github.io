35bc12027125ad669d95fa4c03261908b27d50d0
gaojie <471089443@qq.com>
2024-07-29 18:46:16 +0800

    update

---
+++ assets/Footer-DUpgNOgU.js
@@ -0,0 +1,1 @@
+import{u as o,b as e,c as t}from"./index-DazVQd6w.js";import{_ as s}from"./Header-BHs1vE3g.js";const r={class:"footer"},_={__name:"Footer",setup(c){return o(),(a,n)=>(e(),t("div",r,"© 2022-2024 Gaojie"))}},i=s(_,[["__scopeId","data-v-397d6e13"]]);export{i as F};
