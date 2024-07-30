122e50ff49fd290342d1f6f909678421957f91a8
gaojie <471089443@qq.com>
2024-07-30 12:52:02 +0800

    update

--- assets/ThreeMaterials-Dd9N44HA.js
+++ assets/ThreeMaterials-Dd9N44HA.js
@@ -1,4 +1,4 @@
-import{_ as bf}from"./Header-BHXjEmSj.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-CDK2VeHV.js";/**
+import{_ as bf}from"./Header-C6DPITQ2.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-CCHpR_y-.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
