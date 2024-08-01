d0c2e66c0748152ebc8055e615ea44d93d11d155
gaojie <471089443@qq.com>
2024-08-01 19:45:53 +0800

    update

--- assets/ThreeMaterials-CD5AJcCt.js
+++ assets/ThreeMaterials-CD5AJcCt.js
@@ -1,4 +1,4 @@
-import{_ as bf}from"./Header-C6DPITQ2.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-CCHpR_y-.js";/**
+import{_ as bf}from"./Header-D0GM5Lpy.js";import{r as fl,o as Ef,h as Af,b as wf,c as Tf,e as pl}from"./index-KsuzF6eC.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
