b2ad0f2470246601ae9942278f6bb0480e2663dd
gaojie <471089443@qq.com>
2024-09-09 09:59:57 +0800

    update

--- assets/Footer-HFq8RISh.js
+++
@@ -1,1 +0,0 @@
-import{u as o,b as e,c as t}from"./index-2_S-MIO1.js";import{_ as s}from"./Header-Cf_kKabb.js";const r={class:"footer"},_={__name:"Footer",setup(c){return o(),(a,n)=>(e(),t("div",r,"© 2022-2024 Gaojie"))}},i=s(_,[["__scopeId","data-v-397d6e13"]]);export{i as F};
