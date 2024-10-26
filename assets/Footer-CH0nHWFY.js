1324f49d621496f0a69d531d2321c7eb6635b1b1
gaojie <471089443@qq.com>
2024-10-26 11:56:49 +0800

    update

---
+++ assets/Footer-CH0nHWFY.js
@@ -0,0 +1,1 @@
+import{u as o,a as e,c as t}from"./index-gqDCnyMd.js";import{_ as s}from"./Header-Bkx4FZqY.js";const r={class:"footer"},_={__name:"Footer",setup(a){return o(),(c,n)=>(e(),t("div",r,"© 2022-2024 Gaojie"))}},i=s(_,[["__scopeId","data-v-397d6e13"]]);export{i as F};
