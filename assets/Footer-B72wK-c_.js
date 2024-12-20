48c4275f308000ef57af3fa65837ad4e8c58ac32
gaojie <471089443@qq.com>
2024-12-20 20:09:18 +0800

    update

---
+++ assets/Footer-B72wK-c_.js
@@ -0,0 +1,1 @@
+import{u as o,a as e,c as t}from"./index-CwlV3a58.js";import{_ as s}from"./Header-DLHBCzX6.js";const r={class:"footer"},_={__name:"Footer",setup(a){return o(),(c,n)=>(e(),t("div",r,"© 2022-2024 Gaojie"))}},i=s(_,[["__scopeId","data-v-397d6e13"]]);export{i as F};
