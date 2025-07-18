505caf3def644dea1942739a6878f51022d8fd31
gaojie <471089443@qq.com>
2025-07-18 13:23:30 +0800

    mp3

---
+++ assets/Footer-CMFLbD4v.js
@@ -0,0 +1,1 @@
+import{u as o,a as e,c as t}from"./index-BlLTcYiO.js";import{_ as s}from"./Header-Cr1kn53x.js";const r={class:"footer"},a={__name:"Footer",setup(c){return o(),(_,n)=>(e(),t("div",r,"© 2022-2025 GaoJie"))}},i=s(a,[["__scopeId","data-v-085cbab0"]]);export{i as F};
