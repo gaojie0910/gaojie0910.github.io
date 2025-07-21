ee1a05d51b41408fe45988ab79fc40235e47ac99
gaojie <471089443@qq.com>
2025-07-21 16:06:03 +0800

    mp3
    delete old

---
+++ assets/Footer-DKuuiRg1.js
@@ -0,0 +1,1 @@
+import{u as o,a as e,c as t}from"./index-XTrkusMf.js";import{_ as s}from"./Header-Bkgntq2Y.js";const r={class:"footer"},a={__name:"Footer",setup(c){return o(),(_,n)=>(e(),t("div",r,"© 2022-2025 GaoJie"))}},i=s(a,[["__scopeId","data-v-085cbab0"]]);export{i as F};
