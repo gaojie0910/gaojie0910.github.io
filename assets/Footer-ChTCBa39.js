fb6376cf2b702f105f029e44a56143fe0bb9a2d8
Jason Gao <471089443@qq.com>
2025-09-18 11:33:08 +0800

    fix mp3 bug

---
+++ assets/Footer-ChTCBa39.js
@@ -0,0 +1,1 @@
+import{c as o,d as e}from"./index-DS_TSqO1.js";import{_ as t}from"./Header-DbUNig1X.js";const r={class:"footer"},s={__name:"Footer",setup(_){return(c,a)=>(e(),o("div",r,"© 2024-2025 GaoJie"))}},m=t(s,[["__scopeId","data-v-09042f75"]]);export{m as F};
--- assets/Footer-DAIMqxEV.js
+++
@@ -1,1 +0,0 @@
-import{c as o,d as e}from"./index-DFSk5Jv2.js";import{_ as t}from"./Header-qKw8x96D.js";const r={class:"footer"},s={__name:"Footer",setup(_){return(c,a)=>(e(),o("div",r,"© 2024-2025 GaoJie"))}},m=t(s,[["__scopeId","data-v-09042f75"]]);export{m as F};
